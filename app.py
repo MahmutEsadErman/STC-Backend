2d13c49d797ec630b01bb673fcfce4ca638fa90e
necmettin1461 <39279808+necmettin1461@users.noreply.github.com>
2025-05-07 02:07:51 +0200

    fix

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ def get_timetable(user_id):
                 "breakTime": str(i[4])[:-3] if i[4] else None,
                 "hoursTarget": str(i[5])[:-3] if i[5] else None,
                 "hoursAsIs": str(i[6])[:-3] if i[6] else None,
-                "absence": str(i[7])[:-3] if i[7] else "",
+                "absence": str(i[7]) if i[7] else "no",
                 "comment": str(i[8])[:-3] if i[8] else "",
                 "status": i[9]
             })
